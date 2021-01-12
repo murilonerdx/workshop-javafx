d3520d471cdd9f183d438b80ba8e04dda6c93802
n30config <mu-silva@outlook.com>
2021-01-12 18:01:44 -0300

    Novas atualizacoes

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -1,17 +1,29 @@
 package model.services;
 
-import model.entities.Department;
-
-import java.util.ArrayList;
 import java.util.List;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
 
 public class DepartmentService {
 
-    public List<Department> findAll(){
-        List<Department> list = new ArrayList<>();
-        list.add(new Department(1, "Books"));
-        list.add(new Department(2, "Computers"));
-        list.add(new Department(3, "Movies"));
-        return list;
-    }
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
+	
+	public List<Department> findAll() {
+		return dao.findAll();
+	}
+	
+	public void saveOrUpdate(Department obj) {
+		if (obj.getId() == null) {
+			dao.insert(obj);
+		}
+		else {
+			dao.update(obj);
+		}
+	}
+	
+	public void remove(Department obj) {
+		dao.deleteById(obj.getId());
+	}
 }
