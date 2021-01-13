9a1f4793cc1eacb78e53c0ce2dde3d2e5b62911e
n30config <mu-silva@outlook.com>
2021-01-13 12:26:18 -0300

    Salvando informacoes no banco

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,12 +1,17 @@
 package gui;
 
 
+import db.DbException;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,6 +20,7 @@ public class DepartmentFormController implements Initializable {
 
     private Department entity;
 
+    private DepartmentService service;
 
     @FXML
     private TextField txtId;
@@ -32,10 +38,27 @@ public class DepartmentFormController implements Initializable {
     private Button btCancel;
 
     @FXML
-    public void onBtSaveAction(){
+    public void onBtSaveAction(ActionEvent event){
         if(entity == null){
             throw new IllegalStateException("Entity is null");
+        }if(service == null){
+            throw new IllegalStateException("Service is null");
         }
+        try{
+            entity = getFormData();
+            service.saveOrUpdate(entity);
+            Utils.currentStage(event).close();
+        }catch(DbException e){
+            util.Alerts.showAlert("Error saving obj", null, e.getMessage(), Alert.AlertType.ERROR);
+        }
+
+    }
+
+    private Department getFormData() {
+        Department obj = new Department();
+        obj.setId(Utils.tryParseToInt(txtId.getText()));
+        obj.setName(txtName.getText());
+        return obj;
     }
 
     @FXML
@@ -50,6 +73,10 @@ public class DepartmentFormController implements Initializable {
 
     public void setDepartment(Department entity){
         this.entity = entity;
+    }
+
+    public void setDepartmentService(DepartmentService service){
+        this.service = service;
     }
 
     public void updateFormData(){
